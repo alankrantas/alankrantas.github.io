6ce31eeef909cd445a51b597f410572ece1c3744
alankrantas <alankrantas@users.noreply.github.com>
2022-09-13 15:07:33 +0000

    deploy: ae86fdf0c6ed8147852b846669dfd565b23dba63

--- _app/immutable/chunks/0-ce14e7d2.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-83aa34d1.js";export{t as component};
---
+++ _app/immutable/chunks/1-6439403e.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_error.svelte-e3b05bf0.js";export{t as component};
