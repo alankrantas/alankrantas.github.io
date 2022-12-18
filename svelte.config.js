d3aba4a4bab73351ee93b0bd2437a756c1bccf10
Alan Wang <krantas@gmail.com>
2022-12-18 19:37:42 +0800

    Updating

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -14,7 +16,13 @@ const config = {
 			fallback: 'index.html',
 			precompress: true,
 			strict: true
-		})
+		}),
+		paths: {
+			base: dev ? '' : '/alankrantas.github.io'
+		},
+		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+		// For example, instead of '_app', use 'app_', 'internal', etc.
+		appDir: 'internal'
 	}
 };
 
