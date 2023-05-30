14de28ea93d908d29583dda96dd27fd0a3dfabb6
Alan Wang <krantas@gmail.com>
2023-05-31 00:33:12 +0800

    Updating

--- src/components/ViewItemCard.svelte
+++ src/components/ViewItemCard.svelte
@@ -12,7 +12,7 @@
 <div class="card rounded-4 bg-dark shadow-lg">
 	<img loading="lazy" src={viewItem.imgUrl} class="card-img-top rounded-4" alt={viewItem.name} />
 	<div class="card-body">
-		<div class="card-title text-start text-white display-6 title p-md-md-1 m-md-1">
+		<div class="card-title text-start text-white display-6 title p-md-1 m-md-1">
 			{viewItem.name}
 		</div>
 		<div class="card-text text-start text-white-50 p-md-1 m-md-1">
