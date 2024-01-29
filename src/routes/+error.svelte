c412e480ca19e326351cf19d377bf66553aef784
Alan Wang <krantas@gmail.com>
2024-01-29 02:54:49 +0000

    User updating

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,5 +3,10 @@
 </script>
 
 <div class="alert alert-warning">
-	<span class="display-6 p-md-4 m-md-4">{$page.status}: {$page.error?.message}</span>
+	<p class="display-6 p-md-2 m-md-2">
+		{$page.status}: {$page.error?.message}
+	</p>
+	<p class="p-md-2 m-md-2">
+		<a href="/" class="btn btn-secondary rounded-4">Back</a>
+	</p>
 </div>
--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: 'index.html',
+			fallback: 'error.html',
 			precompress: true,
 			strict: true
 		})
