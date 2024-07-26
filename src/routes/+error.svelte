45d485bee015f89939b6c80cc7f0cc563ca1d2c2
Alan Wang <krantas@gmail.com>
2024-07-26 18:20:02 +0800

    Update +error.svelte

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,16 +5,16 @@
 <div class="alert alert-warning text-center">
 	<p class="display-6 p-md-2 m-md-2">Oops!</p>
 	<p class="code p-md-2 m-md-2">
-		Routing error ({$page.status}: {$page.error?.message || 'Unknown'}).
+		{$page.status}: {$page.error?.message || 'Unknown'}
 	</p>
 	<p class="lead p-md-2 m-md-2">
-		Here, listen to this song, take a long breath, then simple go back.
+		Keep calm and carry on being rickrolled. (Or simply go back :D)
 	</p>
 	<p class="p-md-3 m-md-3">
 		<iframe
 			width="560"
 			height="315"
-			src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=sjnpqqTSXPh5JLvP&amp;autoplay=1&controls=0"
+			src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=sjnpqqTSXPh5JLvP&amp;autoplay=1&mute=1&controls=0"
 			title="You've got rickrolled"
 			frameborder="0"
 			referrerpolicy="strict-origin-when-cross-origin"
