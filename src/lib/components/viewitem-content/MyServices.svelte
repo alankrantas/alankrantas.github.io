4099ffa7bb0bccda2947eca6ecd02f572d792da1
Alan Wang <krantas@gmail.com>
2025-01-26 14:23:58 +0000

    User updating

--- src/lib/components/viewitem-content/MyServices.svelte
+++ src/lib/components/viewitem-content/MyServices.svelte
@@ -45,25 +45,26 @@
 	Generative AIs are useful for general writing; however by design they are trained to write like an <i
 		>average</i
 	> person with fixed knowledge. They cannot reason well in your specific domain without lengthy, detailed
-	prompts, and tend to be unimaginative and repetitive due to the lack of input. Creating reference documents
-	for AIs in the first place can be just as challenging.
+	prompts, and tend to be unimaginative and repetitive due to the lack of input. However, creating reference
+	materials for AI in the first place can be just as challenging.
 </p>
 <p class="indent justify">
 	Quality writing is an art of storytelling, which involves careful researching and organizing
 	around the domain, subject and context, while the creation process is often intuitive, dynamic and
-	nonlinear, much like painting a picture aesthetically, instead of predicting words sequentially.
+	nonlinear, much like painting a picture aesthetically, instead of predicting words sequentially. A
+	good article "feels right" because that's what it should be.
 </p>
 <p class="indent justify">
 	As for translating, a competent translator plays a reliable bridge connecting two worlds
 	containing incompatible cultural concepts and ways of speaking. No languages can be converted <i
 		>word-by-word</i
 	> to another, and only one with sufficient understanding can find you the balanced path of readability
-	and faithfulness.
+	and faithfulness for readers.
 </p>
 <p class="indent justify">
 	Are you looking for someone who can give your business an edge in content differentiation and
 	better user engagement? Are you having trouble with content quality using ChatGPT or alike? Do you
-	believe that genuine content <i>matters</i>? I may be the one to help you to get there.
+	believe that genuine content <i>matters</i>? If so, I may be the one to help you to get there.
 </p>
 <p>
 	<br />
