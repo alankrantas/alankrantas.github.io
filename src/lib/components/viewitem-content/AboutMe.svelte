1ce35b130ebccd8fc2023a02e1f6a39d4ccf14b3
Alan Wang <krantas@gmail.com>
2025-08-05 18:48:48 +0800

    Update AboutMe.svelte

--- src/lib/components/viewitem-content/AboutMe.svelte
+++ src/lib/components/viewitem-content/AboutMe.svelte
@@ -19,15 +19,9 @@
 	My name is <b>{info.name}</b>, currently based in {info.location}.
 </p>
 <p class="indent justify">
-	I am a seasoned technical writer and bilingual translator with over 10 years of experience in
-	software, technology, education, and publishing. Translated, edited, or authored more than 60
-	full-length titles and produced dozens of technical articles and marketing materials.
-</p>
-<p class="indent justify">
-	Expertise spans copywriting, content marketing, software engineering, embedded programming, and
-	AI. Known for rapid adaptability, cross-disciplinary skill integration and strong communication
-	skills to translate complex concepts into accessible content for diverse audiences, and delivered
-	high-impact works that improved user engagement and business growth.
+	I am an experienced writer, editor and translator with 10+ years across software, tech, education, 
+	and publishing with versatile skills. Delivered 60+ full-length translated, edited, or authored titles, 
+	plus a wide range of blog posts, technical articles, and marketing content.
 </p>
 <p class="indent justify">I have personal interests in reading, music, movies and photography.</p>
 <p>
